34e8c458362c845992426b1340aaa7c5b06e0161
Sahil <devinnow8@gmail.com>
2023-03-29 16:49:58 +0530

    Revert "Update index.js"
    This reverts commit 43ba031ff151c343340b026e21468d1de5ef84ec.

--- src/index.js
+++ src/index.js
@@ -28,9 +28,40 @@ const responseBot = async (app) => {
       let userExist = await getResponseData(msg.from);
       try {
         if (msg.type === "button_reply") {
-          // if (msg.data.id === "Welcome_Message") {
-
-          // }
+          if (msg.data.id === "Welcome_Message") {
+            try {
+              const res = await axios.get(
+                process.env.API_END_POINT + "/category-list"
+              );
+              const data = await res.data;
+              if (data) {
+                let tempDataa = { ...userExist.tmp_data };
+                let dataObjj = {
+                  phone_number: msg.from,
+                  type:
+                    userExist.type !== ""
+                      ? visaSequence[visaSequence.indexOf(userExist.type) + 1]
+                      : visaSequence[0],
+                  message:
+                    "Hi, Welcome to OIS appointment booking system. Please select the category",
+                  reply_with: "",
+                  data: JSON.stringify(msg.data),
+                  tmp_data: tempDataa,
+                };
+                const res = await saveResponseData({ ...dataObjj });
+                if (res) {
+                  await bot.sendList(
+                    msg.from,
+                    "Select",
+                    "Hi, Welcome to OIS appointment booking system. Please select the category",
+                    generateText("Welcome_Message", data)
+                  );
+                }
+              }
+            } catch (err) {
+              console.log(err, "err");
+            }
+          }
           if (msg.data.id === "book_appointment") {
             try {
               let apiObj = [
@@ -253,40 +284,6 @@ const responseBot = async (app) => {
           //     console.log(err, "err");
           //   }
           // }
-          if (msg.data.text === "hey") {
-            try {
-              const res = await axios.get(
-                process.env.API_END_POINT + "/category-list"
-              );
-              const data = await res.data;
-              if (data) {
-                let tempDataa = { ...userExist.tmp_data };
-                let dataObjj = {
-                  phone_number: msg.from,
-                  type:
-                    userExist.type !== ""
-                      ? visaSequence[visaSequence.indexOf(userExist.type) + 1]
-                      : visaSequence[0],
-                  message:
-                    "Hi, Welcome to OIS appointment booking system. Please select the category",
-                  reply_with: "",
-                  data: JSON.stringify(msg.data),
-                  tmp_data: tempDataa,
-                };
-                const res = await saveResponseData({ ...dataObjj });
-                if (res) {
-                  await bot.sendList(
-                    msg.from,
-                    "Select",
-                    "Hi, Welcome to OIS appointment booking system. Please select the category",
-                    generateText("Welcome_Message", data)
-                  );
-                }
-              }
-            } catch (err) {
-              console.log(err, "err");
-            }
-          }
           if (userExist.type === "Application_id") {
             let tempDataaaa = {
               ...userExist.tmp_data,
